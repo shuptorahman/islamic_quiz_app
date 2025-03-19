272ceb65e34f9be084d9f5aeae94e6ac67234efa
Shupto Rahman <shuptorahman8@gmail.com>
2025-03-19 21:35:21 +0600

    scoll view added,result added etc

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionSummary extends StatelessWidget {
   const QuestionSummary({
@@ -10,20 +11,115 @@ class QuestionSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children:
-          summaryData.map((data) {
-            return Row(
-              children: [
-                Text(
-                  ((data['question_index']
-                              as int) +
-                          1)
-                      .toString(),
-                ),
-              ],
-            );
-          }).toList(),
+    return SizedBox(
+      height: 400,
+      child: SingleChildScrollView(
+        child: Column(
+          children:
+              summaryData.map((data) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(
+                        vertical: 8,
+                      ),
+                  child: Row(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 30,
+                        width: 30,
+
+                        alignment:
+                            Alignment.center,
+                        decoration: BoxDecoration(
+                          color:
+                              data['user_answer'] ==
+                                      data['correct_answer']
+                                  ? const Color.fromARGB(
+                                    255,
+                                    10,
+                                    154,
+                                    231,
+                                  )
+                                  : const Color.fromARGB(
+                                    255,
+                                    244,
+                                    74,
+                                    156,
+                                  ),
+                          borderRadius:
+                              BorderRadius.circular(
+                                100,
+                              ),
+                        ),
+                        child: Text(
+                          ((data['question_index']
+                                      as int) +
+                                  1)
+                              .toString(),
+                          style: GoogleFonts.lato(
+                            fontWeight:
+                                FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment:
+                              CrossAxisAlignment
+                                  .start,
+                          children: [
+                            Text(
+                              data['question']
+                                  as String,
+                              style:
+                                  GoogleFonts.lato(
+                                    fontWeight:
+                                        FontWeight
+                                            .bold,
+                                    fontSize: 16,
+                                    color:
+                                        Colors
+                                            .white,
+                                  ),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              data['user_answer']
+                                  as String,
+                              style:
+                                  GoogleFonts.lato(
+                                    color:
+                                        Colors
+                                            .white,
+                                  ),
+                            ),
+                            Text(
+                              data['correct_answer']
+                                  as String,
+                              style:
+                                  GoogleFonts.lato(
+                                    color:
+                                        Colors
+                                            .green,
+                                    fontWeight:
+                                        FontWeight
+                                            .bold,
+                                  ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }).toList(),
+        ),
+      ),
     );
   }
 }
--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:islamic_quiz_app/database/question_data.dart';
+import 'package:islamic_quiz_app/question_summary.dart';
 
 class ResultScreen extends StatelessWidget {
   ResultScreen({
@@ -19,8 +21,7 @@ class ResultScreen extends StatelessWidget {
       summary.add({
         'question_index': i,
         'question': questions[i].question,
-        'correct_answer':
-            questions[i].question[0],
+        'correct_answer': questions[i].answers[0],
         'user_answer': choosenAnswers[i],
       });
     }
@@ -29,25 +30,48 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectAnswers =
+        summaryData.where((data) {
+          return data['user_answer'] ==
+              data['correct_answer'];
+        }).length;
+
     return SizedBox(
       width: double.infinity,
       child: Container(
+        margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment:
               MainAxisAlignment.center,
           children: [
             Text(
-              "You answered X out Y questions correctly",
+              "You answered $numCorrectAnswers out $numTotalQuestions questions correctly",
+              style: GoogleFonts.lato(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: const Color.fromARGB(
+                  255,
+                  201,
+                  199,
+                  200,
+                ),
+              ),
+              textAlign: TextAlign.center,
             ),
             SizedBox(height: 30),
-            Text("List of answers and question"),
+            QuestionSummary(
+              summaryData: summaryData,
+            ),
             SizedBox(height: 30),
-            TextButton(
+            TextButton.icon(
               style: TextButton.styleFrom(
                 foregroundColor: Colors.white,
               ),
+              icon: Icon(Icons.refresh),
               onPressed: () {},
-              child: Text(
+              label: Text(
                 "Restart Quiz",
                 style: TextStyle(fontSize: 24),
               ),
